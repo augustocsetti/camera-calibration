04b7249c5788a827e0de162defbc2633e4256b0e
augustocsetti <augustocsetti@live.com>
2022-06-23 02:29:15 -0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pickle
 from datetime import datetime
 
 import cv2
@@ -16,9 +17,22 @@ objp = np.zeros((CHECKERBOARD[0] * CHECKERBOARD[1],3), np.float32)
 objp[:,:2] = np.mgrid[0:CHECKERBOARD[0],0:CHECKERBOARD[1]].T.reshape(-1,2) # x,y coordinates
 
 
-def main(path: str):
+def save_coefficients(mtx, dist, path) -> None:
+    with open(f'{path}/coefficients.p', 'wb') as f:
+        coef = {'mtx': mtx, 'dist': dist}
+        pickle.dump(coef, f)
+
+def load_coefficients(path) -> tuple:
+    '''Reads the coefficients from a path and return it.\n
+    - Return: tuple(mtx, dist)'''
+
+    with open(f'{path}/coefficients.p', 'rb') as f:
+        coef = pickle.load(f)
+    return coef['mtx'], coef['dist']
+
+def main(path: str, correct_files: bool = True):
     '''
-    Function that receives a image file path to calibrate the camera who took these pictures.\n
+    Function that receives a image file path to calibrate the camera that took the pictures.\n
     The function also store the parameters and save the corrected images on results directory.
     '''
 
@@ -30,16 +44,13 @@ def main(path: str):
         print(f'\t{i}')
     print('\n')
 
-    date = datetime.now().strftime('%d-%m-%Y %H-%M')
-    os.makedirs(f'results', exist_ok=True)
-    os.makedirs(f'results/{date}/')
-
     # processing images
+    print('Getting coefficients...')
     for image in tqdm(images):
-
+        
         # openning a image 
         img = cv2.imread(f'{path}{image}')
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # detecting boards on chessboard
         retval, corners = cv2.findChessboardCorners(image=img, patternSize=CHECKERBOARD, flags=None)
@@ -48,23 +59,34 @@ def main(path: str):
         # if borders were detected
         if retval:
             # refine detected boards
-        #     cv2.cornerSubPix(image, corners, winSize, zeroZone, criteria)
+            # corners2 = cv2.cornerSubPix(image, corners, winSize, zeroZone, criteria)
 
             #
             img_points.append(corners)
             object_points.append(objp)  
 
-            #
-            ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(object_points, img_points, img.shape[1:], None, None)
-            # print(ret, mtx, dist, rvecs, tvecs)
-            # salvar parâmetros
 
-            #
+    # calibrate
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(object_points, img_points, img.shape[::-1], None, None)
+    # print(ret, mtx, dist, rvecs, tvecs)
+
+    # creating the results path
+    os.makedirs(f'results', exist_ok=True)
+    date = datetime.now().strftime('%d-%m-%Y %H-%M')
+    destinyPath = path.split('\\')[-2]+date
+    os.makedirs(f'results/{destinyPath}')
+
+    # saving cefficients
+    save_coefficients(mtx, dist, f'results/{destinyPath}')
+
+    # correcting images
+    if correct_files:
+        print('\nUndistort images...')
+        for image in tqdm(images):
+            img = cv2.imread(f'{path}{image}')
+            ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(object_points, img_points, img.shape[1:], None, None)
             img_undistort = cv2.undistort(img, mtx, dist, None, mtx)
-            cv2.imwrite(f'./results/{date}/{image}', img_undistort)
-            # cv2.namedWindow(f'Imagem - {image}', cv2.WINDOW_NORMAL)
-            # cv2.imshow(f'Imagem - {image}', img_undistort, )
-            # cv2.waitKey(0)
+            cv2.imwrite(f'./results/{destinyPath}/{image}', img_undistort)
 
 
 if __name__ == '__main__':
